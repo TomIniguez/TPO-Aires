1f2a364f0fa790cda8a2336ba0c49cbba80d6d7e
TomIniguez <Lightningstom@yahoo.com>
2022-11-11 10:35:21 -0300

    Test

--- TPO/src/Model/Tecnico.java
+++ TPO/src/Model/Tecnico.java
@@ -1,5 +1,43 @@
 package Model;
 
-public class Tecnico {
+import java.util.List;
 
+public class Tecnico  extends Agenda{
+	
+	private List<Instalacion> listaInstalacionAsig;
+	private String nombre;
+	private String apellido;
+	private int id;
+	
+	public List<Instalacion> getListaInstalacionAsig() {
+		return listaInstalacionAsig;
+	}
+	
+	public void setListaInstalacionAsig(List<Instalacion> listaInstalacionAsig) {
+		this.listaInstalacionAsig = listaInstalacionAsig;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellido() {
+		return apellido;
+	}
+	
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
 }
